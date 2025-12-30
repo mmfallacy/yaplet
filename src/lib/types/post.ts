68026565fe61f1d7d217b325aa255a74ad06a454
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-12-30 16:59:25 +0800

    feat: set posts part of a thread in thread's meta.json instead of manifest.json

--- src/lib/types/post.ts
+++ src/lib/types/post.ts
@@ -1,37 +1,38 @@
 export interface Post {
-  id: string;
-  content: string;
-  createdAt: string;
-  threadId: string | null;
-  threadOrder?: number;
-  images: string[];
-  likes: number;
+	id: string;
+	content: string;
+	createdAt: string;
+	threadId: string | null;
+	threadOrder?: number;
+	images: string[];
+	likes: number;
 }
 
 export interface Thread {
-  id: string;
-  title: string;
-  description: string;
-  createdAt: string;
+	id: string;
+	title: string;
+	description: string;
+	createdAt: string;
+	posts: string[];
 }
 
 export interface PostWithThread extends Post {
-  thread?: Thread;
+	thread?: Thread;
 }
 
 export interface Comment {
-  id: string;
-  postId: string;
-  userId: string;
-  userName: string;
-  userAvatar?: string;
-  content: string;
-  createdAt: string;
+	id: string;
+	postId: string;
+	userId: string;
+	userName: string;
+	userAvatar?: string;
+	content: string;
+	createdAt: string;
 }
 
 export interface User {
-  id: string;
-  name: string;
-  avatar?: string;
-  githubUsername?: string;
+	id: string;
+	name: string;
+	avatar?: string;
+	githubUsername?: string;
 }
