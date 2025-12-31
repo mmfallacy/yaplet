cf33bbbc7908b07de7d4ba3d7357764a7e5230ac
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-01-01 04:50:26 +0800

    chore: add .env.example and devshell helper script to generate webhook secrets

--- nix/devShell.nix
+++ nix/devShell.nix
@@ -6,18 +6,27 @@ with pkgs;
 mkShell {
   name = "yaplet development shell";
 
-  nativeBuildInputs = [
-    extras.pkgs-unstable.bun
-    nodejs_24
-    vtsls
-    vscode-langservers-extracted
-    emmet-language-server
-    tailwindcss-language-server
-    svelte-language-server
-  ]
-  ++ pkgs.lib.mapAttrsToList (k: v: pkgs.writeShellScriptBin k v) {
-    pnpm = "corepack pnpm \$@";
-    pnpx = "corepack pnpx \$@";
-  };
+  nativeBuildInputs =
+    let
+      bin = pkg: lib.getExe pkg;
+      gen-webhook-secret = pkgs.writeShellScriptBin "gen-webhook-secret" ''
+        ${bin pkgs.openssl} rand -base64 32
+      '';
+    in
+    [
+      extras.pkgs-unstable.bun
+      nodejs_24
+      vtsls
+      vscode-langservers-extracted
+      emmet-language-server
+      tailwindcss-language-server
+      svelte-language-server
+
+      gen-webhook-secret
+    ]
+    ++ pkgs.lib.mapAttrsToList (k: v: pkgs.writeShellScriptBin k v) {
+      pnpm = "corepack pnpm \$@";
+      pnpx = "corepack pnpx \$@";
+    };
 
 }
