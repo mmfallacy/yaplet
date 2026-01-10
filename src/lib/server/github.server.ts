1b60093f2b20696bf90366c0bc6a3a7bf9974d44
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-01-10 15:43:30 +0800

    feat: create fetchFromGithubApi

--- src/lib/server/github.server.ts
+++ src/lib/server/github.server.ts
@@ -17,3 +17,25 @@ export async function fetchContent(path: string): Promise<Response> {
 
 	return response;
 }
+
+const API_CONTENT_BASE_URL = 'https://api.github.com/repos/mmfallacy/yaplet-content/contents/';
+const API_CONTENT_REF = 'main';
+
+export async function fetchFromGithubApi(path: string): Promise<Response> {
+	const url = new URL(path, API_CONTENT_BASE_URL);
+	url.searchParams.set('ref', API_CONTENT_REF);
+
+	const response = await fetch(url, {
+		headers: {
+			Authorization: `Bearer ${GITHUB_PAT}`,
+			Accept: 'application/vnd.github.object+json',
+			'X-GitHub-Api-Version': '2022-11-28'
+		}
+	});
+
+	if (!response.ok) {
+		throw new Error(`Failed to fetch ${path}: ${response.status} ${response.statusText}`);
+	}
+
+	return response;
+}
