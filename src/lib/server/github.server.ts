c74cff444a4ede40cd816830cbb5e6144a7a5eb4
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-01-11 02:25:26 +0800

    feat: github.server.ts cache

--- src/lib/server/github.server.ts
+++ src/lib/server/github.server.ts
@@ -1,4 +1,5 @@
 import { GITHUB_PAT, CONTENT_BASE_URL } from '$env/static/private';
+import assert from 'node:assert';
 
 export async function fetchContent(path: string): Promise<Response> {
 	const url = new URL(path, CONTENT_BASE_URL);
@@ -21,21 +22,43 @@ export async function fetchContent(path: string): Promise<Response> {
 const API_CONTENT_BASE_URL = 'https://api.github.com/repos/mmfallacy/yaplet-content/contents/';
 const API_CONTENT_REF = 'main';
 
+const MemoryCache = new Map<string, { etag: string; data: Response }>();
+
 export async function fetchFromGithubApi(path: string): Promise<Response> {
 	const url = new URL(path, API_CONTENT_BASE_URL);
 	url.searchParams.set('ref', API_CONTENT_REF);
+
+	const entry = MemoryCache.get(path);
+	console.log(entry);
 
 	const response = await fetch(url, {
 		headers: {
 			Authorization: `Bearer ${GITHUB_PAT}`,
 			Accept: 'application/vnd.github.object+json',
-			'X-GitHub-Api-Version': '2022-11-28'
+			'X-GitHub-Api-Version': '2022-11-28',
+			'If-None-Match': `${entry?.etag ?? 'v1'}`
 		}
 	});
+
+	console.log(`FETCH [${path}]: ${response.status} ${response.statusText}`);
+	console.log(response.headers);
+
+	if (response.status == 304) {
+		console.log('Matched Etag, using cache');
+		assert(typeof entry !== 'undefined');
+		return entry.data;
+	}
 
 	if (!response.ok) {
 		throw new Error(`Failed to fetch ${path}: ${response.status} ${response.statusText}`);
 	}
+
+	const etag = response.headers.get('etag') || '';
+	console.log(etag);
+	MemoryCache.set(path, {
+		etag,
+		data: response
+	});
 
 	return response;
 }
