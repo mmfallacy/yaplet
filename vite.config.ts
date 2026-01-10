eed690cd2de78ee43c25fe3a6490d97e3d990b20
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-01-10 15:35:18 +0800

    chore: enable nixos.local as allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		// whitelist my local dev environment as host
+		allowedHosts: ['nixos.local']
+	}
 });
