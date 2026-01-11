62eab4611312e8bde994bbcb04d4119921b284fc
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-01-12 06:38:03 +0800

    feat: sort manifest by latest post first

--- src/routes/api/v1/feed/+server.ts
+++ src/routes/api/v1/feed/+server.ts
@@ -2,12 +2,19 @@ import { ManifestSchema } from '$lib/schema';
 import { fetchFromGithubApi } from '$lib/server/github.server';
 import { json } from '@sveltejs/kit';
 
+function toDateMs(a: string) {
+	return new Date(a).getTime();
+}
+
 export async function GET() {
 	const data = await fetchFromGithubApi('content/manifest.v1.json');
 
 	const decoded = Buffer.from(data.content, 'base64').toString('utf-8');
 	const parsed = JSON.parse(decoded);
 	const manifest = ManifestSchema.parse(parsed);
+
+	// Sort Descending (i.e. latest post first)
+	manifest.sort((a, b) => toDateMs(b.createdAt) - toDateMs(a.createdAt));
 
 	return json(manifest);
 }
