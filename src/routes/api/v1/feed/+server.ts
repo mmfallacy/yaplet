e2d26dc3cdf1323d50683d451ea599f443f49e4b
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-01-12 06:53:52 +0800

    feat: cull manifest by search params offset and limit

--- src/routes/api/v1/feed/+server.ts
+++ src/routes/api/v1/feed/+server.ts
@@ -1,20 +1,35 @@
 import { ManifestSchema } from '$lib/schema';
 import { fetchFromGithubApi } from '$lib/server/github.server';
-import { json } from '@sveltejs/kit';
+import { json, type RequestHandler } from '@sveltejs/kit';
+import z from 'zod/v4';
 
 function toDateMs(a: string) {
 	return new Date(a).getTime();
 }
 
-export async function GET() {
+const QuerySchema = z.object({
+	offset: z.coerce.number().int().positive().default(0),
+	limit: z.coerce.number().int().positive().optional()
+});
+
+export const GET: RequestHandler = async ({ url }) => {
+	const q = QuerySchema.parse(Object.fromEntries(url.searchParams));
+
 	const data = await fetchFromGithubApi('content/manifest.v1.json');
 
 	const decoded = Buffer.from(data.content, 'base64').toString('utf-8');
 	const parsed = JSON.parse(decoded);
 	const manifest = ManifestSchema.parse(parsed);
 
-	// Sort Descending (i.e. latest post first)
-	manifest.sort((a, b) => toDateMs(b.createdAt) - toDateMs(a.createdAt));
+	// end = q.offset + q.limit; if q.limit undefined, then end = undefined so slice end is until end of array
+	let end = q.limit;
+	if (end) end += q.offset;
 
-	return json(manifest);
-}
+	// Sort Descending (i.e. latest post first)
+	// NOTE: Array.prototype.sort returns reference to same array.
+	const slice = manifest
+		.sort((a, b) => toDateMs(b.createdAt) - toDateMs(a.createdAt))
+		.slice(q.offset, end);
+
+	return json(slice);
+};
