4b4e44d856fa64b31c9a282a4f9b8d43205bd782
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-01-14 03:12:57 +0800

    feat: api/v1/post/[id]

---
+++ src/routes/api/v1/post/[id]/+server.ts
@@ -0,0 +1,26 @@
+import { error, json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { fetchFromGithubApi } from '$lib/server/github.server';
+import { normalizeMarkdownFilename } from '$lib/utils';
+import matter from 'gray-matter';
+import { PostSchema } from '$lib/schema';
+
+export const GET: RequestHandler = async ({ params }) => {
+	const id = params.id;
+	try {
+		const data = await fetchFromGithubApi(`content/standalone/${normalizeMarkdownFilename(id)}`);
+		console.log(data);
+		const decoded = Buffer.from(data.content, 'base64').toString('utf-8');
+		const parsed = matter(decoded);
+		const transformed = PostSchema.parse({
+			type: 'standalone',
+			id,
+			...parsed.data,
+			content: parsed.content
+		});
+
+		return json(transformed);
+	} catch {
+		return error(500, `Something went wrong fetching post ${id}`);
+	}
+};
