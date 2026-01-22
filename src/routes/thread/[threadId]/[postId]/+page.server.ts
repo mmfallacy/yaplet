052f082bf2022b90951d3815d5df02392909163d
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-01-23 02:00:00 +0800

    docs: add note on why we do not reuse parent load function

--- src/routes/thread/[threadId]/[postId]/+page.server.ts
+++ src/routes/thread/[threadId]/[postId]/+page.server.ts
@@ -2,6 +2,12 @@ import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { getPostById } from '$lib/server/post/getPostById';
 
+// Note: I am not reusing parent load functions here for the following reason:
+// - Performance benefit only exists when we navigate from /thread/threadId to /thread/threadId/postId
+// - Large detriment if otherwise! Why? Since we need to resolve the whole posts array before I can get the current post
+// The current implementation just refetches the post, which seems like a better deal for both cases.
+// Not too much overhead to refetch as it should go through the same memory cache,
+// Fetch as needed instead of fetching whole posts array only to pick one.
 export const load: PageServerLoad = async ({ params }) => {
 	const basePath = `content/threads/${params.threadId}`;
 
